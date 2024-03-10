7ae2039943268b6dee360ab031180fbde39a6360
Stefano Borzì <stefanoborzi32@gmail.com>
2024-03-10 19:12:53 +0100

    fix: replace deprecated visit world object method

--- src/mod_phased_duels.cpp
+++ src/mod_phased_duels.cpp
@@ -53,7 +53,7 @@ public:
             std::list<Player*> playerList;
             acore::AnyPlayerInObjectRangeCheck checker(go, 100.0f);
             acore::PlayerListSearcher<acore::AnyPlayerInObjectRangeCheck> searcher(go, playerList, checker);
-            go->VisitNearbyWorldObject(100.0f, searcher);
+            Cell::VisitWorldObjects(go, searcher, 100.0f);
 
             // insert players' phases to used phases, ignore GMs
             uint32 usedPhases = 0;
